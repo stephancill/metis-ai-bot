0038f867f544afe0a2279c87fdeb7d176aab6509
Stephan Cilliers <stephanus.cilliers@gmail.com>
2018-01-01 20:39:57 +0200

    Add support for environement vars in config

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
+import os
+
 class Config:
 	follow_ids = ["914663684928139264"] # @metis_ai
 
 	# Spend a fraction of BTC balance per signal (default 10%)
-	btc_spend_multiplier = 0.1
+	try:
+		btc_spend_multiplier = float(os.environ["btc_spend_multiplier"])
+	except:
+		btc_spend_multiplier = 0.1
 	
 	# Price to sell at 
 	# buy * sell_multiplier = sell price (default 1.1)
-	sell_multiplier = 1.1
+	try:
+		sell_multiplier = float(os.environ["sell_multiplier"])
+	except:
+		sell_multiplier = 1.1
 
 	# Buy price multiplier - price ahead of pump (default 1.02)
-	buy_multiplier = 1.02
+	try:
+		buy_multiplier = float(os.environ["buy_multiplier"])
+	except:
+		buy_multiplier = 1.02
 
